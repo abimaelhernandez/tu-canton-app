729e2b50e24ba09b58745d104d3bba5dbd9e1e50
abimaelhernandez <hernandezabimael364@gmail.com>
2019-07-23 23:34:34 -0500

    made first drop down,now need to populate with data from db

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,12 +9,33 @@ export default class App extends Component {
       equipos : 0
     }
   }
+
+getTeams = () => {
+  console.log("getting teams");
+}
   render(){
     console.log("this is equipos state :", this.state.equipos);
     return(
-        <div>
-          <h1> Hola mundo</h1>
-          <p>estamos aqui </p>
+        <div className="App">
+          <div className="parent-container">
+            <div className="getTeamContainer">
+              <button className="getTeamsButton" onClick={this.getTeams}> get teams </button>
+            </div>
+
+            <div className="dropDownContainer">
+              <div className="dropdown">
+                <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Dropdown button
+                </button>
+                <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                  <a className="dropdown-item" href="#">Action</a>
+                  <a className="dropdown-item" href="#">Another action</a>
+                  <a className="dropdown-item" href="#">Something else here</a>
+                </div>
+              </div>
+            </div>
+
+          </div>
         </div>
     )
   }
